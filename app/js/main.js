68250568bf54200a9a783105bb3aa1c7d724ebfe
moderno <ilyenkov401@gmail.com>
2020-05-24 15:19:29 +0300

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function () {
+$(function() {
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
 
+    $(".product-slider__inner").slick({
+        arrows: false,
+        dots: true,
+        slidesToScroll: 4,
+        slidesToShow: 4,
+    });
+
+    var mixer = mixitup(".products__inner-box");
 });
